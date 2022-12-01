cc1f0fcae16195fcf4c6f3080d01534f93fcfd4e
jonsam <jonsam.ng@foxmail.com>
2022-12-01 12:51:43 +0800

    feat: 🆕 update resume page

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -529,3 +529,19 @@ export const LinkIcon = (
 		></path>
 	</svg>
 );
+
+export const ExternalLinkIcon = (
+	<svg
+		t="1669868721681"
+		class="icon"
+		viewBox="0 0 1024 1024"
+		version="1.1"
+		xmlns="http://www.w3.org/2000/svg"
+		p-id="2669"
+	>
+		<path
+			d="M719.168 207.168L576 64h384v384l-150.272-150.272-264.128 264.064-90.496-90.496 264.064-264.128zM192 960H64V64h384v128H192v640h640V576h128v384H192z"
+			p-id="2670"
+		></path>
+	</svg>
+);
--- src/routes/resume/resume.tsx
+++ src/routes/resume/resume.tsx
@@ -1,5 +1,20 @@
 import { component$, useStyles$ } from "@builder.io/qwik";
+import { ExternalLinkIcon } from "~/components/icon";
 import styles from "./resume.less";
+
+export const LearnMore = component$(
+	({ link, self = false }: { link: string; self?: boolean }) => {
+		return (
+			<a
+				class="btn btn-text more"
+				href={link}
+				target={self ? "_self" : "_blank"}
+			>
+				了解更多<i class="icon">{ExternalLinkIcon}</i>
+			</a>
+		);
+	}
+);
 
 export default component$(() => {
 	useStyles$(styles);
@@ -90,12 +105,58 @@ export default component$(() => {
 			<h2 id="edu">
 				<span>教育背景</span>
 			</h2>
-			<section></section>
+			<section>
+				<div class="block">
+					<p class="title">
+						<span>
+							<a href="https://www.xpu.edu.cn/" target="_blank">
+								西安工程大学
+							</a>
+						</span>
+						<span>软件工程（本科）</span>
+						<span>2016.09-2020.07</span>
+						<span>
+							<LearnMore link="/about/edu" />
+						</span>
+					</p>
+					<p>
+						主修课程：Java语言程序设计、C语言程序设计、网络原理、数据库原理、数据结构、操作系统、算法设计、计算机组成原理、软件工程等。
+					</p>
+				</div>
+			</section>
 
 			<h2>
 				<span>工作经历</span>
 			</h2>
-			<section id="working"></section>
+			<section id="working">
+				<div class="block">
+					<p class="title">
+						<span>
+							<a href="https://www.xylink.com/" target="_blank">
+								小鱼易连
+							</a>
+						</span>
+						<span>前端开发（业务部）</span>
+						<span>2020.07-至今</span>
+						<span>
+							<LearnMore link="/project/?#working-project" />
+						</span>
+					</p>
+					<ul class="vertical mark">
+						<li>
+							负责客户端产品内嵌H5公有云、私有云中部分模块的开发、重构、改版和维护工作。
+						</li>
+						<li>
+							负责教育平台、CRM平台、企培平台等平台项目中部分模块的开发和维护工作。
+						</li>
+						<li>
+							负责开发者平台、日志分析平台等内部平台项目中项目搭建、开发和上线工作。
+						</li>
+						<li>负责教育业务WebRTC巡课、监课业务的开发、重构和维护工作。</li>
+						<li>负责内部组件库、工具库的搭建、开发、推动工作。</li>
+					</ul>
+				</div>
+			</section>
 
 			<h2 id="project">
 				<span>专业技能</span>
