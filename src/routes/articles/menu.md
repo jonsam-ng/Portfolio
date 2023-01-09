a239a4eac1a1510e814763f210bb8142dbceb5e7
jonsam <jonsam.ng@foxmail.com>
2023-01-09 21:37:48 +0800

    docs: 📖 update contact

--- src/routes/articles/menu.md
+++ src/routes/articles/menu.md
@@ -8,3 +8,4 @@
 
 - [平台项目经验总结](post/platform_project_sum/index.mdx)
 - [我对项目重构的理解](post/rechore_project/index.mdx)
+- [从巡课项目总结项目重构经验](post/patrol_project/index.mdx)
--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -1,5 +1,6 @@
 import { component$, useStylesScoped$ } from "@builder.io/qwik";
 import { DocumentHead } from "@builder.io/qwik-city";
+import { MailIcon, CallIcon } from "~/components/icon";
 import styles from "./index.less?inline";
 
 /**
@@ -9,7 +10,35 @@ import styles from "./index.less?inline";
 export default component$(() => {
 	useStylesScoped$(styles);
 
-	return <div class="content">Contact</div>;
+	return (
+		<div class="content">
+			<div class="banner">
+				<div class="left">
+					<p class="title">联系我</p>
+					<p class="detail">您可以通过邮件或者电话随时随地与我沟通。</p>
+				</div>
+				<div class="right">
+					<img src="/asset/image/contact.png" />
+				</div>
+			</div>
+			<div class="info">
+				<ul>
+					<li>
+						<i class="icon">{MailIcon}</i>
+						<a href="mailto:jonsam.ng@foxmail.com" target="_blank">
+							jonsam.ng@foxmail.com
+						</a>
+					</li>
+					<li>
+						<i class="icon">{CallIcon}</i>
+						<a href="tel:15829313329" target="_blank">
+							15829313329
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 });
 
 export const head: DocumentHead = {
